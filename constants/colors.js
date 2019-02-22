ee6bae767a171a2c273b3becc00aa8a411038f73
Chris Queen <chrisqueen10@gmail.com>
2019-02-22 18:21:56 -0500

    Added copy and basic structing + styling to the Check In screen

--- constants/colors.js
+++ constants/colors.js
@@ -1,3 +1,6 @@
 export const Colors = {
-  lightBlue: '#49a5f9'
+  blue: '#49a5f9',
+  lightBlue: '#cee7ff',
+  red: '#DB5C5C',
+  lightRed: '#f9c5c5'
 }
--- screens/CheckIn.js
+++ screens/CheckIn.js
@@ -1,16 +1,103 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { StyleSheet, View, Text, ScrollView, Image } from 'react-native';
+import { ScreenContainerStyles } from '../styles/baseStyles';
+import { Colors } from '../constants/colors';
+
 export default class CheckIn extends React.Component {
+
   static navigationOptions = {
     title: 'Check In',
+    headerStyle: {
+      backgroundColor: Colors.red,
+      borderBottomWidth: 0
+    },
+    headerTintColor: Colors.lightRed,
+    headerTitleStyle: {
+      color: Colors.lightRed
+    },
   };
 
   render() {
-
     return (
-      <View>
-        <Text>CheckIn</Text>
+      <View style={[styles.screenContainer, ScreenContainerStyles]}>
+        <ScrollView>
+          <Image style={styles.sectionImage} source={require('../assets/checkin.png')}></Image>
+          <View style={styles.sectionContainer}>
+            <Text style={styles.sectionHeader}>1. Sitting in a chair or a comfortable place, take a moment to feel...</Text>
+            <View style={styles.sectionBodyContainer}>
+              <Text style={styles.sectionBodyText}>Contact with the floor</Text>
+              <Text style={styles.sectionBodyText}>Your spine in a line</Text>
+              <Text style={styles.sectionBodyText}>Your hands in your lap</Text>
+              <Text style={styles.sectionBodyText}>Your heart to the sky</Text>
+            </View>
+          </View>
+          <View style={styles.sectionContainer}>
+            <Text style={styles.sectionHeader}>2. Check into your body</Text>
+            <View style={styles.sectionBodyContainer}>
+              <Text style={styles.sectionBodyText}>Are you feeling tight or relaxed?</Text>
+              <Text style={styles.sectionBodyText}>Any tingling or pain?</Text>
+              <Text style={styles.sectionBodyText}>Feeling comfortable or uneasy?</Text>
+              <Text style={styles.sectionBodyText}>Any tightness in your face or shoulders?</Text>
+              <Text style={styles.sectionBodyText}>Close your eyes and monitor how you are feeling physically and breathe</Text>
+            </View>
+          </View>
+          <View style={styles.sectionContainer}>
+            <Text style={styles.sectionHeader}>3. Check into your thoughts</Text>
+            <View style={styles.sectionBodyContainer}>
+              <Text style={styles.sectionBodyText}>Do you notice many thoughts?</Text>
+              <Text style={styles.sectionBodyText}>Do you feel space and calmness?</Text>
+              <Text style={styles.sectionBodyText}>Are they past, present, or future?</Text>
+              <Text style={styles.sectionBodyText}>Close your eyes and place your attention on your breath</Text>
+            </View>
+          </View>
+          <View style={styles.sectionContainer}>
+            <Text style={styles.sectionHeader}>4. Check in to how you are feeling emotionally</Text>
+            <View style={styles.sectionBodyContainer}>
+              <Text style={styles.sectionBodyText}>Do you feel joy, anger, or content?</Text>
+              <Text style={styles.sectionBodyText}>Feeling anxious, nervous, or excited?</Text>
+              <Text style={styles.sectionBodyText}>Bored or enthusiastic?</Text>
+              <Text style={styles.sectionBodyText}>Grateful or worried?</Text>
+              <Text style={styles.sectionBodyText}>Close your eyes, name the emotion (for example "happy, happy, happy")</Text>
+            </View>
+          </View>
+        </ScrollView>
       </View>
     );
   }
 }
+
+
+const styles = StyleSheet.create({
+  screenContainer: {
+    backgroundColor: Colors.red,
+    padding: 30
+  },
+  sectionContainer: {
+    marginTop: 30,
+    marginBottom: 30
+  },
+  sectionHeader: {
+    color: Colors.lightRed,
+    fontWeight: 'bold',
+    fontSize: 25
+  },
+  sectionImage: {
+    flex: 1,
+    width: null,
+    resizeMode: 'contain',
+    marginTop: 10,
+    marginBottom: 10
+  },
+  sectionBodyContainer: {
+    marginTop: 25,
+    marginBottom: 15,
+    lineHeight: 30
+  },
+  sectionBodyText: {
+    fontSize: 22,
+    color: 'white',
+    marginTop: 10,
+    marginBottom: 10,
+    fontWeight: 'bold'
+  }
+});
