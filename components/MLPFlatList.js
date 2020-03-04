7c074e8832cd9075135509c7566437db5fad74d4
Chris Queen <chrisqueen10@gmail.com>
2020-03-04 08:01:25 -0500

    Fixed most linter errors for MLPFlatList.js

--- components/MLPFlatList.js
+++ components/MLPFlatList.js
@@ -1,48 +1,13 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import { View, TouchableOpacity, Text, FlatList, StyleSheet } from 'react-native'
-import { getLightThemeColorByCategory, getDarkThemeColorByCategory } from '../utils/categoryValues'
+import { getDarkThemeColorByCategory } from '../utils/categoryValues'
 import Colors from '../constants/colors'
 
-export default class MLPFlatList extends React.Component {
-  constructor(props) {
-    super(props)
-
-    this.renderRow = this.renderRow.bind(this)
-  }
-
-  renderRow(rowData) {
-    const bottomBorderStyle = rowData.index == 0 ? styles.firstBorder : null
-    const { category } = this.props
-    return (
-      <View>
-        <TouchableOpacity onPress={() => this.props.onRowPressHandler(rowData)}>
-          <View
-            style={[
-              styles.rowContainer,
-              bottomBorderStyle,
-              { borderColor: getDarkThemeColorByCategory(category) },
-            ]}
-          >
-            <Text style={[styles.rowTitle, { color: 'white' }]}>{rowData.item.title}</Text>
-          </View>
-        </TouchableOpacity>
-      </View>
-    )
-  }
-
-  render() {
-    return (
-      <FlatList
-        keyExtractor={(item, index) => `key:${index}`}
-        data={this.props.listData}
-        renderItem={this.renderRow}
-        contentContainerStyle={{ paddingBottom: 100 }}
-      />
-    )
-  }
-}
-
 const styles = StyleSheet.create({
+  contentContainer: {
+    paddingBottom: 100,
+  },
   firstBorder: {
     borderTopWidth: 1,
   },
@@ -58,3 +23,51 @@ const styles = StyleSheet.create({
     paddingLeft: 30,
   },
 })
+
+class MLPFlatList extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.renderRow = this.renderRow.bind(this)
+  }
+
+  renderRow(rowData) {
+    const bottomBorderStyle = rowData.index === 0 ? styles.firstBorder : null
+    const { category, onRowPressHandler } = this.props
+    return (
+      <View>
+        <TouchableOpacity onPress={() => onRowPressHandler(rowData)}>
+          <View
+            style={[
+              styles.rowContainer,
+              bottomBorderStyle,
+              { borderColor: getDarkThemeColorByCategory(category) },
+            ]}
+          >
+            <Text style={[styles.rowTitle, { color: Colors.white }]}>{rowData.item.title}</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
+    )
+  }
+
+  render() {
+    const { listData } = this.props
+    return (
+      <FlatList
+        keyExtractor={(_, index) => `key:${index}`}
+        data={listData}
+        renderItem={this.renderRow}
+        contentContainerStyle={styles.contentContainer}
+      />
+    )
+  }
+}
+
+MLPFlatList.propTypes = {
+  category: PropTypes.string.isRequired,
+  onRowPressHandler: PropTypes.func.isRequired,
+  listData: PropTypes.object.isRequired,
+}
+
+export default MLPFlatList
