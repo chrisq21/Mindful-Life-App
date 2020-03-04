9f2dd3b960514df5effdb124119f3c628935666c
Chris Queen <chrisqueen10@gmail.com>
2020-03-03 20:54:39 -0500

    Fixed linter errors in Learn.js

--- screens/Learn.js
+++ screens/Learn.js
@@ -1,77 +1,17 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import { ScrollView, View, Text, Image, StyleSheet, Dimensions } from 'react-native'
-import { Video } from 'expo'
 import VideoPlayer from '../components/VideoPlayer'
 import { ScreenContainerStyles } from '../styles/baseStyles'
 import { spanishCopy, englishCopy } from '../constants/copy'
 import { Colors } from '../constants/colors'
 import DrawerIcon from '../components/DrawerIcon'
-
-export default class Learn extends React.Component {
-  static navigationOptions = ({ navigation }) => {
-    const category = navigation.getParam('category', '')
-    return {
-      title: 'Learn',
-      drawerLabel: 'About',
-      headerRight: <DrawerIcon navigation={navigation} />,
-      headerStyle: {
-        backgroundColor: Colors.blue,
-        borderBottomWidth: 0,
-      },
-      headerTintColor: Colors.lightBlue,
-      headerTitleStyle: {
-        color: Colors.lightBlue,
-      },
-    }
-  }
-
-  render() {
-    const { navigation } = this.props
-    const language = navigation.getParam('language', 'english')
-    const copyData = language === 'spanish' ? spanishCopy : englishCopy
-    const { width } = Dimensions.get('window')
-    const videoSource =
-      language === 'spanish'
-        ? require('../assets/animation_spanish.mp4')
-        : require('../assets/animation.mp4')
-
-    return (
-      <View style={{ backgroundColor: Colors.blue }}>
-        <ScrollView>
-          <View style={ScreenContainerStyles}>
-            <View style={styles.sectionContainer}>
-              <View style={styles.paddingContainer}>
-                <Text style={styles.sectionHeader}>{copyData.learnScreen.whatIsHeader}</Text>
-              </View>
-              <VideoPlayer source={videoSource} width={width} />
-              <View style={styles.paddingContainer}>
-                <Text style={styles.sectionBodyText}>{copyData.learnScreen.whatIsBody}</Text>
-              </View>
-            </View>
-            <Image style={styles.sectionImage} source={require('../assets/meditating.png')} />
-            <View style={[styles.sectionContainer, styles.paddingContainer]}>
-              <Text style={styles.sectionHeader}>Benefits</Text>
-              <Text style={styles.sectionBodyText}>{copyData.learnScreen.benefitsBody1}</Text>
-              <Text style={styles.sectionBodyText}>{copyData.learnScreen.benefitsBody2}</Text>
-              <Image style={styles.sectionImage} source={require('../assets/clouds.png')} />
-            </View>
-            <View style={[styles.sectionContainer, styles.paddingContainer]}>
-              <Text style={styles.sectionHeader}>Posters</Text>
-              <Image
-                style={[styles.sectionImage, styles.posterImage]}
-                source={require('../assets/poster.png')}
-              />
-              <Image
-                style={[styles.sectionImage, styles.posterImage]}
-                source={require('../assets/together.png')}
-              />
-            </View>
-          </View>
-        </ScrollView>
-      </View>
-    )
-  }
-}
+import animatedVideoSrc from '../assets/animation.mp4'
+import animatedVideoSpanishSrc from '../assets/animation_spanish.mp4'
+import meditationImgSrc from '../assets/meditating.png'
+import cloudsImgSrc from '../assets/clouds.png'
+import posterImgSrc from '../assets/poster.png'
+import togetherImgSrc from '../assets/together.png'
 
 const styles = StyleSheet.create({
   paddingContainer: {
@@ -83,7 +23,7 @@ const styles = StyleSheet.create({
     width: null,
   },
   sectionBodyText: {
-    color: 'white',
+    color: Colors.white,
     fontSize: 25,
     lineHeight: 40,
     marginBottom: 15,
@@ -107,3 +47,68 @@ const styles = StyleSheet.create({
     width: null,
   },
 })
+
+class Learn extends React.Component {
+  static navigationOptions = ({ navigation }) => {
+    return {
+      title: 'Learn',
+      drawerLabel: 'About',
+      headerRight: <DrawerIcon navigation={navigation} />,
+      headerStyle: {
+        backgroundColor: Colors.blue,
+        borderBottomWidth: 0,
+      },
+      headerTintColor: Colors.lightBlue,
+      headerTitleStyle: {
+        color: Colors.lightBlue,
+      },
+    }
+  }
+
+  render() {
+    const { navigation } = this.props
+    const language = navigation.getParam('language', 'english')
+    const copyData = language === 'spanish' ? spanishCopy : englishCopy
+    const { width } = Dimensions.get('window')
+    const videoSource = language === 'spanish' ? animatedVideoSpanishSrc : animatedVideoSrc
+
+    return (
+      <View style={{ backgroundColor: Colors.blue }}>
+        <ScrollView>
+          <View style={ScreenContainerStyles}>
+            <View style={styles.sectionContainer}>
+              <View style={styles.paddingContainer}>
+                <Text style={styles.sectionHeader}>{copyData.learnScreen.whatIsHeader}</Text>
+              </View>
+              <VideoPlayer source={videoSource} width={width} />
+              <View style={styles.paddingContainer}>
+                <Text style={styles.sectionBodyText}>{copyData.learnScreen.whatIsBody}</Text>
+              </View>
+            </View>
+            <Image style={styles.sectionImage} source={meditationImgSrc} />
+            <View style={[styles.sectionContainer, styles.paddingContainer]}>
+              <Text style={styles.sectionHeader}>Benefits</Text>
+              <Text style={styles.sectionBodyText}>{copyData.learnScreen.benefitsBody1}</Text>
+              <Text style={styles.sectionBodyText}>{copyData.learnScreen.benefitsBody2}</Text>
+              <Image style={styles.sectionImage} source={cloudsImgSrc} />
+            </View>
+            <View style={[styles.sectionContainer, styles.paddingContainer]}>
+              <Text style={styles.sectionHeader}>Posters</Text>
+              <Image style={[styles.sectionImage, styles.posterImage]} source={posterImgSrc} />
+              <Image style={[styles.sectionImage, styles.posterImage]} source={togetherImgSrc} />
+            </View>
+          </View>
+        </ScrollView>
+      </View>
+    )
+  }
+}
+
+Learn.propTypes = {
+  navigation: PropTypes.shape({
+    getParam: PropTypes.func,
+    navigate: PropTypes.func,
+  }).isRequired,
+}
+
+export default Learn
