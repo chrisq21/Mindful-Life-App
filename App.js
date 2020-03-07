79e74f4f771fdd4243edaf888e817096ae4e06be
Chris Queen <chrisqueen10@gmail.com>
2020-03-06 23:52:58 -0500

    Fixed linter errors in App.js

--- App.js
+++ App.js
@@ -1,19 +1,8 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
 import AppNavigator from './navigation/AppNavigator'
 
-export default class App extends React.Component {
-  render() {
-    return <AppNavigator />
-  }
+function App() {
+  return <AppNavigator />
 }
 
-const styles = StyleSheet.create({
-  container: {
-    alignItems: 'center',
-    backgroundColor: '#fff',
-    flex: 1,
-    fontFamily: 'Aclonica',
-    justifyContent: 'center',
-  },
-})
+export default App
