c5f74b156a196910ddaec818d16c0f2ce99a1c1b
Chris Queen <chrisqueen10@gmail.com>
2020-03-07 14:51:06 -0500

    AppNavigator logic is now component based instead of object based

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -1,36 +1,46 @@
-import { createAppContainer } from 'react-navigation'
-import { createDrawerNavigator } from 'react-navigation-drawer'
-import { createStackNavigator } from 'react-navigation-stack'
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+// import { createDrawerNavigator } from 'react-navigation-drawer'
 import About from '../components/About'
 import HomeScreen from '../screens/Home'
 import LanguageSelection from '../screens/LanguageSelection'
 import PlaylistsList from '../screens/PlaylistsList'
 import TracksList from '../screens/TracksList'
-import AudioPlayerScreen from '../screens/AudioPlayer'
-import LearnScreen from '../screens/Learn'
-import CheckInScreen from '../screens/CheckIn'
+import AudioPlayer from '../screens/AudioPlayer'
+import Learn from '../screens/Learn'
+import CheckIn from '../screens/CheckIn'
 
-const MainNavigator = createStackNavigator({
-  LanguageSelection: { screen: LanguageSelection },
-  Home: { screen: HomeScreen },
-  PlaylistsList: { screen: PlaylistsList },
-  TracksList: { screen: TracksList },
-  AudioPlayer: { screen: AudioPlayerScreen },
-  Learn: { screen: LearnScreen },
-  CheckIn: { screen: CheckInScreen },
-})
+// TODO: Bring back drawer navigator
 
-const DrawerNavigator = createDrawerNavigator(
-  {
-    MainNavigator: { screen: MainNavigator },
-  },
-  {
-    drawerPosition: 'right',
-    contentComponent: About,
-    drawerLockMode: 'locked-closed',
-  }
-)
+// const DrawerNavigator = createDrawerNavigator(
+//   {
+//     MainNavigator: { screen: MainNavigator },
+//   },
+//   {
+//     drawerPosition: 'right',
+//     contentComponent: About,
+//     drawerLockMode: 'locked-closed',
+//   }
+// )
 
-const AppNavigator = createAppContainer(DrawerNavigator)
+const Stack = createStackNavigator()
+
+function AppNavigator() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="LanguageSelection">
+        <Stack.Screen name="LanguageSelection" component={LanguageSelection} />
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="About" component={About} />
+        <Stack.Screen name="Learn" component={Learn} />
+        <Stack.Screen name="CheckIn" component={CheckIn} />
+        <Stack.Screen name="PlaylistsList" component={PlaylistsList} />
+        <Stack.Screen name="TracksList" component={TracksList} />
+        <Stack.Screen name="AudioPlayer" component={AudioPlayer} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
 
 export default AppNavigator
