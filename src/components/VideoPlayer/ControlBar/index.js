f1c081d53b9b82a299ee9aa098c997e5472601f7
Chris Queen <chrisqueen10@gmail.com>
2020-04-01 12:37:35 -0400

    Added ControlBar component folder to for video controls. Also added loading state image for the video

---
+++ src/components/VideoPlayer/ControlBar/index.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import styled from 'styled-components/native'
+import Colors from '../../../styles/Colors'
+import { MaterialIcons } from '@expo/vector-icons'
+
+const ControlBarWrapper = styled.View`
+  width: 100%;
+  background-color: ${Colors.blackHalfOpacity};
+  flex-direction: row;
+  justify-content: center;
+`
+
+function ControlBar({ isPlaying, toggleVideo }) {
+  return (
+    <ControlBarWrapper>
+      <MaterialIcons
+        name={isPlaying ? 'pause' : 'play-arrow'}
+        size={45}
+        color={Colors.white}
+        onPress={toggleVideo}
+      />
+    </ControlBarWrapper>
+  )
+}
+
+ControlBar.propTypes = {
+  isPlaying: PropTypes.bool.isRequired,
+  toggleVideo: PropTypes.func.isRequired,
+}
+
+export default ControlBar
--- src/components/VideoPlayer/index.js
+++ src/components/VideoPlayer/index.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import { View } from 'react-native'
 import { Video } from 'expo-av'
+import ControlBar from './ControlBar'
+import LoadingImageSrc from './assets/loading-image.png'
 
 function VideoPlayer({ source }) {
+  const [isPlaying, setIsPlaying] = useState(true)
+
+  const toggleVideo = () => {
+    setIsPlaying(!isPlaying)
+  }
+
   return (
     <View>
       <Video
@@ -12,10 +20,14 @@ function VideoPlayer({ source }) {
         volume={1.0}
         isMuted={false}
         resizeMode="contain"
-        shouldPlay
+        shouldPlay={isPlaying}
         isLooping
         style={{ width: '100%', aspectRatio: 1.78 }}
+        usePoster={true}
+        posterSource={LoadingImageSrc}
+        posterStyle={{ height: '100%', width: '100%' }}
       />
+      <ControlBar isPlaying={isPlaying} toggleVideo={toggleVideo} />
     </View>
   )
 }
