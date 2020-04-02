cda766466f5f75803ce5c33ac871a9eaecfbc322
Chris Queen <chrisqueen10@gmail.com>
2020-04-01 20:19:01 -0400

    Split home component file into a component folder with index.js and styles.js

---
+++ src/screens/Home/index.js
@@ -0,0 +1,62 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { spanishCopy, englishCopy } from '../../constants/copy'
+import questionMarkSrc from '../../assets/question-mark.png'
+import anchorImgSrc from '../../assets/anchor.png'
+import smileImgSrc from '../../assets/smile.png'
+import headphoneImgSrc from '../../assets/headphones.png'
+import ScreenWrapper from '../../components/Shared/ScreenWrapper'
+import {
+  LearnButton,
+  LearnButtonText,
+  CheckInButton,
+  CheckInButtonText,
+  SitsButton,
+  SitsButtonText,
+  HipHopButton,
+  HipHopButtonText,
+  ImageWrapper,
+} from './styles'
+
+function Home({ navigation, route }) {
+  const { language } = route.params
+  const copyData = language === 'spanish' ? spanishCopy : englishCopy
+
+  return (
+    <ScreenWrapper>
+      <LearnButton onPress={() => navigation.navigate('Learn', { language })}>
+        <LearnButtonText>{copyData.homeScreen.learn}</LearnButtonText>
+        <ImageWrapper source={questionMarkSrc} />
+      </LearnButton>
+      <CheckInButton onPress={() => navigation.navigate('CheckIn', { language })}>
+        <CheckInButtonText>{copyData.homeScreen.checkIn}</CheckInButtonText>
+        <ImageWrapper source={anchorImgSrc} />
+      </CheckInButton>
+      <SitsButton
+        onPress={() => navigation.navigate('PlaylistsList', { category: 'sits', language })}
+      >
+        <SitsButtonText>{copyData.homeScreen.sits}</SitsButtonText>
+        <ImageWrapper source={smileImgSrc} />
+      </SitsButton>
+      <HipHopButton
+        onPress={() => navigation.navigate('PlaylistsList', { category: 'hip-hop', language })}
+      >
+        <HipHopButtonText>{copyData.homeScreen.hipHop}</HipHopButtonText>
+        <ImageWrapper source={headphoneImgSrc} />
+      </HipHopButton>
+    </ScreenWrapper>
+  )
+}
+
+Home.propTypes = {
+  navigation: PropTypes.shape({
+    navigate: PropTypes.func,
+  }).isRequired,
+  route: PropTypes.shape({
+    params: PropTypes.shape({
+      language: PropTypes.string.isRequired,
+    }).isRequired,
+  }).isRequired,
+}
+
+export default Home
---
+++ src/screens/Home/styles.js
@@ -0,0 +1,58 @@
+import Colors from '../../constants/colors'
+import styled from 'styled-components/native'
+
+const SectionButton = styled.TouchableOpacity`
+  flex: 1;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  border-bottom-width: 1px;
+  padding-right: 20px;
+  padding-left: 20px;
+`
+
+const ButtonText = styled.Text`
+  color: ${Colors.white};
+  flex: 2;
+  font-size: 30px;
+  font-weight: bold;
+  margin-left: 20px;
+`
+
+export const LearnButton = styled(SectionButton)`
+  background-color: ${Colors.blue};
+`
+
+export const LearnButtonText = styled(ButtonText)`
+  color: ${Colors.lightBlue};
+`
+
+export const CheckInButton = styled(SectionButton)`
+  background-color: ${Colors.red};
+`
+
+export const CheckInButtonText = styled(ButtonText)`
+  color: ${Colors.lightRed};
+`
+
+export const SitsButton = styled(SectionButton)`
+  background-color: ${Colors.green};
+`
+
+export const SitsButtonText = styled(ButtonText)`
+  color: ${Colors.lightGreen};
+`
+
+export const HipHopButton = styled(SectionButton)`
+  background-color: ${Colors.orange};
+`
+
+export const HipHopButtonText = styled(ButtonText)`
+  color: ${Colors.lightOrange};
+`
+export const ImageWrapper = styled.Image`
+  align-self: center;
+  flex: 1;
+  height: 50%;
+  resize-mode: contain;
+`
