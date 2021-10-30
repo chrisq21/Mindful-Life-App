22353cb8aa7468726a2cce705dcf6c4e6c54246d
Chris Queen <chrisqueen10@gmail.com>
2021-10-30 18:45:50 -0400

    Open soundcloud in webview

--- src/lib/sound-cloud-services.js
+++ src/lib/sound-cloud-services.js
@@ -7,6 +7,12 @@ export const userURL = {
   'sits-spanish': `${baseUserURL}/user-825440555&client_id=${CLIENT_ID}`,
 }
 
+export const soundCloudUrlMap = {
+  'hip-hop': 'https://soundcloud.com/mindful-life-hip-hop/sets',
+  sits: 'http://soundcloud.com/mindful-life-project/sets',
+  'sits-spanish': 'http://soundcloud.com/user-825440555/sets',
+}
+
 /**
  *
  * @param {string} category
--- src/screens/Playlists/index.js
+++ src/screens/Playlists/index.js
@@ -1,78 +1,29 @@
-import React, { useEffect, useState } from 'react'
-import { Alert } from 'react-native'
+import React from 'react'
 import PropTypes from 'prop-types'
-import styled, { ThemeProvider } from 'styled-components/native'
-import LoadingSpinner from '../../components/shared/LoadingSpinner'
-import List from '../../components/shared/List'
-import { ListWrapper, Heading } from '../../components/shared/ListScreenStyles'
-import { fetchUser, fetchPlaylists } from '../../lib/sound-cloud-services'
+import { ThemeProvider } from 'styled-components/native'
+import { soundCloudUrlMap } from '../../lib/sound-cloud-services'
+import { WebView } from 'react-native-webview'
 import themes from '../../styles/themes'
 
-const LoadingSpinnerWrapper = styled(LoadingSpinner)`
-  flex-direction: row;
-  flex-grow: 1;
-  justify-content: center;
-  align-items: center;
-`
+const webViewStyles = {
+  width: '100%',
+  height: '100%',
+}
 
-function Playlists({ route, navigation }) {
-  const [playlists, setPlaylists] = useState(null)
+function Playlists({ route }) {
+  const { category, language } = route.params
 
-  const onRowPress = (selectedPlaylist) => {
-    const { category } = route.params
-    // Filter out non-streamable tracks
-    const streamableTracks = selectedPlaylist.tracks.filter((track) => track.streamable)
-    navigation.navigate('Tracks', {
-      tracks: streamableTracks,
-      playlistTitle: selectedPlaylist.title,
-      category,
-    })
+  let soundCloudUrl = soundCloudUrlMap['sits']
+
+  if (language === 'spanish' && category === 'sits') {
+    soundCloudUrl = soundCloudUrlMap['sits-spanish']
+  } else {
+    soundCloudUrl = soundCloudUrlMap[category]
   }
-
-  useEffect(() => {
-    /**
-     * @description Fetch user data, then playlist data based off of the user id
-     * @effect Call setPlaylists state hook with new playlist data
-     */
-    const { category, language } = route.params
-    const fetchData = async () => {
-      try {
-        const user = await fetchUser(category, language)
-        if (user && user.id) {
-          const playlistsData = await fetchPlaylists(user.id)
-          if (playlistsData) {
-            setPlaylists(playlistsData)
-          }
-        }
-      } catch (error) {
-        if (error) {
-          Alert.alert(
-            'Error',
-            'Unable to retreive SoundCloud data. Please restart the app and try again.',
-            [{ text: 'OK' }],
-            { cancelable: true }
-          )
-        }
-      }
-    }
-
-    fetchData()
-  }, [])
-
-  const { category } = route.params
 
   return (
     <ThemeProvider theme={themes[category]}>
-      <ListWrapper>
-        {!playlists && <LoadingSpinnerWrapper />}
-        {playlists && (
-          <>
-            {/* eslint-disable react-native/no-raw-text */}
-            <Heading>Playlists</Heading>
-            <List category={category} listData={playlists} onRowPress={onRowPress} />
-          </>
-        )}
-      </ListWrapper>
+      <WebView style={webViewStyles} source={{ uri: soundCloudUrl }} />
     </ThemeProvider>
   )
 }
@@ -83,9 +34,6 @@ Playlists.propTypes = {
       category: PropTypes.string,
       language: PropTypes.string,
     }).isRequired,
-  }).isRequired,
-  navigation: PropTypes.shape({
-    navigate: PropTypes.func,
   }).isRequired,
 }
 
