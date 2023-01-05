003672eaf08955905dbd61904c6e1a86ddd48083
Chris Queen <chrisqueen10@gmail.com>
2023-01-04 19:20:51 -0500

    styled about page

--- src/screens/About/styles.js
+++ src/screens/About/styles.js
@@ -3,30 +3,27 @@ import styled from 'styled-components/native'
 import Colors from '../../styles/colors'
 
 export const AboutScreen = styled(View)`
-  background-color: ${Colors.blue};
-  padding: 18px;
+  background-color: ${Colors.white};
+  padding: 20px 15px;
 `
 
 export const AboutText = styled(Text)`
   color: ${Colors.black};
   font-size: 20px;
   line-height: 25px;
-`
-
-export const AboutTextWithVerticalMargins = styled(AboutText)`
-  margin: 15px 0;
+  margin: 20px 0;
 `
 
 export const LogoImage = styled(Image)`
+  margin-top: 40px;
+  maring-bottom: 30px;
   width: 100%;
-  margin: 10px 0;
   resize-mode: contain;
 `
 
 export const LinksWrapper = styled(View)`
-  align-items: center;
   padding-bottom: 10px;
-  padding-top: 10px;
+  text-align: left;
 `
 
 export const Link = styled(AboutText)`
@@ -35,4 +32,5 @@ export const Link = styled(AboutText)`
   line-height: 30px;
   margin-bottom: 20px;
   text-decoration-line: underline;
+  text-align: left;
 `
